bc16e2f1695ff2f93250d9e8f219dd4912d24087
Talevski <talevskikristijan@gmail.com>
2019-07-08 20:18:43 +0200

    HW2-IntroWith3Options

---
+++ VRProject/Assets/Scripts/MoveInstr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInstr : MonoBehaviour
+{
+    public bool flag = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveText()
+    {
+        if (flag)
+        {
+            RectTransform myRectTransform = GetComponent<RectTransform>();
+            myRectTransform.localPosition -= Vector3.right;
+            flag = false;
+        }
+        else
+        {
+            RectTransform myRectTransform = GetComponent<RectTransform>();
+            myRectTransform.localPosition += Vector3.right;
+            flag = true;
+        }
+        
+    }
+}
