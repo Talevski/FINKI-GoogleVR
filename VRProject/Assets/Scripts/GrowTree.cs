572505b75ef9dd80344b1f34f7bc5db0184c3c7f
Talevski <talevskikristijan@gmail.com>
2019-07-13 18:57:33 +0200

    HW3-Interaction

---
+++ VRProject/Assets/Scripts/GrowTree.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowTree : MonoBehaviour
+{
+    public void growTree()
+    {
+        GetComponent<Animator>().SetBool("isAnimating", true);
+    }
+}
---
+++ VRProject/Assets/Scripts/OpenCloseBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseBall : MonoBehaviour
+{
+    Vector3 rot = Vector3.zero;
+    float rotSpeed = 40f;
+    Animator anim;
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        gameObject.transform.eulerAngles = rot;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openCloseBall()
+    {
+        if (!anim.GetBool("Open_Anim"))
+        {
+            anim.SetBool("Open_Anim", true);
+        }
+        else
+        {
+            anim.SetBool("Open_Anim", false);
+        }
+    }
+}
