e07ce94eae5b9816ad30c6b6223cf38f59fefcc4
Talevski <talevskikristijan@gmail.com>
2019-07-14 11:27:58 +0200

    HW5-GameManager

--- VRProject/Assets/Scripts/GrowTree.cs
+++ VRProject/Assets/Scripts/GrowTree.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GrowTree : MonoBehaviour
 {
-    public AudioSource audioSource;
+    private AudioSource audioSource;
+    private AudioClip audioClip;
+    private bool isGrown = false;
 
     public void Start()
     {
         audioSource = GetComponentInParent<AudioSource>();
+        audioClip = audioSource.clip;
     }
     public void growTree()
     {
         GetComponent<Animator>().SetBool("isAnimating", true);
-        audioSource.PlayOneShot(audioSource.clip, 1f);
+        audioSource.PlayOneShot(audioClip, 1f);
+        //GetComponentInParent<SentenceAssembler>().GetSentence(0);
+
+        if(isGrown == false){
+            GameManager.instance.incrementScore();
+
+            int score = GameManager.instance.getScore();
+            if(score <= 13){
+                GetComponentInParent<SentenceAssembler>().GetSentence(score);
+            }
+            else{
+                GetComponentInParent<SentenceAssembler>().GetSentence(14);
+            }
+
+            isGrown = true;
+        }
+        //audioSource.PlayOneShot(audioSource.clip, 1f);
+        
     }
 }
